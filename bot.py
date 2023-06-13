b7994bcd8f348e0e466a9e72a3e72a69fb876871
Ravshanbekk07 <ramazonof07@gmail.com>
2023-06-13 15:42:03 +0500

    fsdknfjasd

--- bot.py
+++ bot.py
@@ -6,10 +6,10 @@ from telegram.ext import (
     CallbackQueryHandler,
 )
 from callback_functions import (
-    start,brands
+    start, brands
 
 )
-from db import DB,get_phones_by_brend
+from db import DB, get_phones_by_brend
 db = DB(file_name='db.json')
 
 TOKEN = os.environ.get('TOKEN')
@@ -19,27 +19,30 @@ def main():
     updater = Updater(token=TOKEN)
     dispatcher = updater.dispatcher
 
-    dispatcher.add_handler(handler=CommandHandler('start',start))
-    dispatcher.add_handler(handler=CallbackQueryHandler(brands, pattern = "mavjud brendlar"))
-    dispatcher.add_handler(handler=CallbackQueryHandler(db.get_phones_by_brend, pattern = "Oppo"))
-    dispatcher.add_handler(handler=CallbackQueryHandler(get_phones_by_brend, pattern = "Vivo"))
-    dispatcher.add_handler(handler=CallbackQueryHandler(get_phones_by_brend, pattern = "Huawei"))
-    dispatcher.add_handler(handler=CallbackQueryHandler(get_phones_by_brend, pattern = "Samsung"))
-    dispatcher.add_handler(handler=CallbackQueryHandler(get_phones_by_brend, pattern = "Apple"))
-    dispatcher.add_handler(handler=CallbackQueryHandler(get_phones_by_brend, pattern = "Nokia"))
-    dispatcher.add_handler(handler=CallbackQueryHandler(get_phones_by_brend, pattern = "Mi"))
-    dispatcher.add_handler(handler=CallbackQueryHandler(get_phones_by_brend, pattern = "Redmi"))
-    dispatcher.add_handler(handler=CallbackQueryHandler(get_phones_by_brend, pattern = "Xiaomi"))
-
-
+    dispatcher.add_handler(handler=CommandHandler('start', start))
+    dispatcher.add_handler(handler=CallbackQueryHandler(
+        brands, pattern="mavjud brendlar"))
+    dispatcher.add_handler(handler=CallbackQueryHandler(
+        db.get_phones_by_brend, pattern="Oppo"))
+    dispatcher.add_handler(handler=CallbackQueryHandler(
+        get_phones_by_brend, pattern="Vivo"))
+    dispatcher.add_handler(handler=CallbackQueryHandler(
+        get_phones_by_brend, pattern="Huawei"))
+    dispatcher.add_handler(handler=CallbackQueryHandler(
+        get_phones_by_brend, pattern="Samsung"))
+    dispatcher.add_handler(handler=CallbackQueryHandler(
+        get_phones_by_brend, pattern="Apple"))
+    dispatcher.add_handler(handler=CallbackQueryHandler(
+        get_phones_by_brend, pattern="Nokia"))
+    dispatcher.add_handler(handler=CallbackQueryHandler(
+        get_phones_by_brend, pattern="Mi"))
+    dispatcher.add_handler(handler=CallbackQueryHandler(
+        get_phones_by_brend, pattern="Redmi"))
+    dispatcher.add_handler(handler=CallbackQueryHandler(
+        get_phones_by_brend, pattern="Xiaomi"))
 
     updater.start_polling()
     updater.idle()
 
+
 main()
-
-
-
-
-
-
