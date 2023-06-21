7fb77062c11343e1585fc3c0b2e0df8845642231
Ravshanbekk07 <ramazonof07@gmail.com>
2023-06-21 07:24:42 +0500

    cool

--- bot.py
+++ bot.py
@@ -1,34 +1,51 @@
 import os
 from telegram.ext import (
-    Updater, CommandHandler,
+    CommandHandler,
     MessageHandler, Filters,
     CallbackQueryHandler,
+    Dispatcher
 )
 from callback_functions import (
     start, brands,
     send_products, send_product_detail,remove,add_cart,my_cards,kontaktlar,haqimizda,clear
 
 )
+from telegram import Bot,Update
+from flask import Flask,request
+
+app = Flask(__name__)
 
 TOKEN = os.environ.get('TOKEN')
+bot = Bot(TOKEN)
 
-
+@app.route('/webhook/',methods = ['POST'])
 def main():
-    updater = Updater(token=TOKEN)
-    dispatcher = updater.dispatcher
+    
+    dispatcher = Dispatcher(bot,None,workers =0)
+    data = request.get_json(force=True)
+    update = Update.de_json(data=data,bot=bot)
+
     dispatcher.add_handler(handler=CommandHandler('start', start))
-    dispatcher.add_handler(handler=MessageHandler(filters=Filters.text('📓mavjud brendlar'), callback=brands)) 
     dispatcher.add_handler(handler=CallbackQueryHandler(send_products, pattern="brend:"))
     dispatcher.add_handler(handler=CallbackQueryHandler(send_product_detail, pattern="product:"))
     dispatcher.add_handler(handler=CallbackQueryHandler(remove, pattern="remove"))
     dispatcher.add_handler(handler=CallbackQueryHandler(add_cart,pattern='add-cart:'))
     dispatcher.add_handler(handler=CallbackQueryHandler(clear, pattern="clear cart"))
 
+    dispatcher.add_handler(handler=MessageHandler(filters=Filters.text('📓mavjud brendlar'), callback=brands)) 
     dispatcher.add_handler(handler=MessageHandler(filters=Filters.text('🛒my cart'), callback=my_cards))
     dispatcher.add_handler(handler=MessageHandler(filters=Filters.text('☎️contact'),callback = kontaktlar))
     dispatcher.add_handler(handler=MessageHandler(filters=Filters.text('📝about'),callback = haqimizda))
     
+    dispatcher.process_update(update)
+    return 'cool'
 
-    updater.start_polling()
-    updater.idle()
-main()
+@app.route('/')
+def home():
+    return 'running'
+
+@app.route('/set-webhook/')
+def set_hook():
+    r = bot.set_webhook("https://stanger.pythonanywhere.com/webhook/")
+    return f'info:{r}'
+
--- callback_functions.py
+++ callback_functions.py
@@ -2,7 +2,7 @@ from telegram import (
     Update,
     InlineKeyboardMarkup,
     InlineKeyboardButton,
-    KeyboardButton,ReplyKeyboardMarkup
+    KeyboardButton, ReplyKeyboardMarkup
 )
 from telegram.ext import CallbackContext
 from db import DB
@@ -11,12 +11,12 @@ db = DB(file_name='db.json')
 
 
 def start(update: Update, context: CallbackContext) -> None:
-   
-    db.add_user(update.message.chat.id,update.message.chat.first_name,update.message.chat.username)
+
+    db.add_user(update.message.chat.id,
+                update.message.chat.first_name, update.message.chat.username)
     first_name = update.message.chat.first_name
 
     # btn1= InlineKeyboardButton(text = "mavjud brendlar", callback_data="mavjud brendlar")
-    
 
     # inline_keyboard = [[btn1]]
 
@@ -27,74 +27,81 @@ def start(update: Update, context: CallbackContext) -> None:
     #     text=f"Hello, <b>{first_name}</b>. Xush Kelibsiz \n Bu-Telefon savdo \n Sahifani tanlang 👇🏼",
     #     reply_markup=InlineKeyboardMarkup(inline_keyboard=inline_keyboard)
 
-
     btn1 = KeyboardButton("📓mavjud brendlar")
     btn2 = KeyboardButton('🛒my cart')
     btn3 = KeyboardButton('☎️contact')
     btn4 = KeyboardButton('📝about')
-    
-    reply_keyboard =[[btn1,btn2],
-                     [btn3,btn4],
-                     
-                     ]
+
+    reply_keyboard = [[btn1, btn2],
+                      [btn3, btn4],
+
+                      ]
 
     update.message.reply_html(
-                             text= f"Hello, <b>{first_name}</b>. Xush Kelibsiz \n Bu-Telefon savdo \n Sahifani tanlang 👇🏼",
-                             reply_markup=ReplyKeyboardMarkup(keyboard=reply_keyboard,resize_keyboard=True)) 
-    
+        text=f"Hello, <b>{first_name}</b>. Xush Kelibsiz \n Bu-Telefon savdo \n Sahifani tanlang 👇🏼",
+        reply_markup=ReplyKeyboardMarkup(keyboard=reply_keyboard, resize_keyboard=True))
+
 
 def brands(update: Update, context: CallbackContext):
     inline_keyboards = []
     for brend in db.get_brends():
-        btn1 = InlineKeyboardButton(text=brend , callback_data=f"brend:{brend}")
+        btn1 = InlineKeyboardButton(text=brend, callback_data=f"brend:{brend}")
         inline_keyboards.append([btn1])
     # for btn in inline_keyboards:
     #     if btn!=inline_keyboards[0][0]:
     #         inline_keyboards.append([btn])
     update.message.reply_text(
-    text = f"bizda mavjud brendlar quyidagilar ⏬",
-    reply_markup=InlineKeyboardMarkup(inline_keyboard=inline_keyboards)
+        text=f"bizda mavjud brendlar quyidagilar ⏬",
+        reply_markup=InlineKeyboardMarkup(inline_keyboard=inline_keyboards)
     )
+
 
 def send_products(update: Update, context: CallbackContext):
     brend = update.callback_query.data.split(":")[1]
     products = db.get_phones_by_brend(brend)
     inline_keyboards = []
     for product in products:
-        btn1 = InlineKeyboardButton(text=product['name'] , callback_data=f"product:{product.doc_id}")
+        btn1 = InlineKeyboardButton(
+            text=product['name'], callback_data=f"product:{product.doc_id}")
         inline_keyboards.append([btn1])
 
     update.callback_query.message.reply_text(
-        text = f"bizda {brend} brendiga tegishli mahsulotlar quyidagilar ⏬",
+        text=f"bizda {brend} brendiga tegishli mahsulotlar quyidagilar ⏬",
         reply_markup=InlineKeyboardMarkup(inline_keyboard=inline_keyboards)
     )
 
+
 def send_product_detail(update: Update, context: CallbackContext):
-    
+
     product_id = int(update.callback_query.data.split(":")[1])
-    btn1 = InlineKeyboardButton(text='add cart', callback_data=f'add-cart:{product_id}')
+    btn1 = InlineKeyboardButton(
+        text='add cart', callback_data=f'add-cart:{product_id}')
     btn2 = InlineKeyboardButton(text='remove', callback_data='remove')
-    inline_keyboards = [[btn1,btn2]]
+    inline_keyboards = [[btn1, btn2]]
     product = db.get_phones_by_id(product_id)
     update.callback_query.message.reply_photo(
         photo=product['image'],
-        caption = f"bizda {product['name']} mahsuloti quyidagilar ⏬\n\nbrendi: {product['brend']}\nrang: {product['color']}\nram: {product['ram']}\nxotira: {product['memory']}\nnarxi: {product['price']}",
+        caption=f"bizda {product['name']} mahsuloti quyidagilar ⏬\n\nbrendi: {product['brend']}\nrang: {product['color']}\nram: {product['ram']}\nxotira: {product['memory']}\nnarxi: {product['price']}",
         reply_markup=InlineKeyboardMarkup(inline_keyboard=inline_keyboards)
     )
+
 
 def remove(update: Update, context: CallbackContext):
     update.callback_query.message.delete()
 
+
 def add_cart(update: Update, context: CallbackContext):
-    chat_id  = update.callback_query.message.chat.id
+    chat_id = update.callback_query.message.chat.id
     product_id = update.callback_query.data.split(':')[1]
-    db.add_cart(chat_id,product_id)
+    db.add_cart(chat_id, product_id)
     update.callback_query.message.reply_text(
-        text = f"savatga qoshildi ",
-       
+        text=f"savatga qoshildi ",
+
     )
+
+
 def my_cards(update: Update, context: CallbackContext):
-    chat_id  = update.message.chat.id
+    chat_id = update.message.chat.id
     products = db.get_my_cart(chat_id)
 
     text = ''
@@ -106,14 +113,20 @@ def my_cards(update: Update, context: CallbackContext):
     # create keyboard
     inline_keyboard = [[btn1]]
 
+    if text == '':
+        text = 'savatcha bosh'
     update.message.reply_text(
-        text = text,
+        text=text,
         reply_markup=InlineKeyboardMarkup(inline_keyboard=inline_keyboard)
- 
+
     )
+
+
 def clear(update: Update, context: CallbackContext):
-    chat_id  = update.message.chat.id
+    chat_id = update.callback_query.message.chat.id
     db.clear_cart(chat_id)
+    update.callback_query.message.reply_text('savatcha tozalanadi')
+    update.callback_query.message.delete()
 
 
 def kontaktlar(update: Update, context: CallbackContext):
@@ -132,8 +145,9 @@ def kontaktlar(update: Update, context: CallbackContext):
         reply_markup=InlineKeyboardMarkup(inline_keyboard=inline_keyboard)
     )
 
+
 def haqimizda(update: Update, context: CallbackContext):
-     update.message.reply_html(
+    update.message.reply_html(
         text=f"<b>Biz haqimizda: </b>\n biz <i>Phone shop</i> azolari ushbu botni\n 10.06.2023 da sizga manfaatli bolishi uchun tashkil qilganmiz.\n Biz orqali telefon sotib olganingizdan song\n bizni ozimiz sizga bonus sifatida dastavka xizmatlarini korsatamiz\n <b>faqatgina</b> siz 'contact' tugmasini bosib biz bilan aloqaga chiqishingiz darkor "
-       
+
     )
